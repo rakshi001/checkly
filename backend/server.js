d8a48c6e7ecf7e799a8e6a735f590c74ab09a605
Rakshith <mlrakshi1@gmail.com>
2025-08-26 19:34:54 +0530

    added new endpoints

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,25 @@ import express from "express";
 const app = express();
 
 app.get("/api", (req, res) => {
-  res.status(200).res.send("hi there");
+  res.json({
+    body: "hi there how are u",
+  });
 });
 // get post put delete
-// to create
 
-app.post("/api", (req, res) => {
-  res.status(200).res.send("your note created successfully");
-  console.log("hey backend")
-});
+app.post('/api/notes', (req,res)=>{
+  res.status(201).json({
+    message:"hi buddy post created successfully!"
+  })
+})
+
+app.post('/api/notes', (req,res)=>{
+  res.status(200).json({
+    message:"hi buddy post created successfully!"
+  })
+})
+
+
 
 app.listen(5001, () => {
   console.log("server started at 5001");
