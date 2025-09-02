562f9c7286766fbb793fd64c1ea98f37f3f34423
Rakshith <mlrakshi1@gmail.com>
2025-09-02 23:15:02 +0530

    .

---
+++ backend/routes/notesRoutes.js
@@ -0,0 +1,7 @@
+const notesRoutes = (req, res) => {
+  res.status(200).json({
+    msg: "how are u doing",
+  });
+};
+
+export default notesRoutes;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import express from "express";
-import noteRoutes from "./routes/notesRoutes.js"
+import noteRoutes from "./routes/notesRoutes.js";
 
 const app = express();
 
-app.use("/api/notes",noteRoutes)
+app.use("/api/notes", noteRoutes);
 
-
+app.post("/api/notes", (req, res) => {
+  res.status(201).json({
+    msg: "it is a post req",
+  });
+});
 
 app.listen(5001, () => {
-  console.log("server sta rted at 5001");
+  console.log("server started at 5001");
 });
