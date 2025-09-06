ec45c61c1d28c76442bac102be45e7fb8ed799d9
Rakshith <mlrakshi1@gmail.com>
2025-09-06 19:43:54 +0530

    connected db

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,18 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
 
+import dotenv from "dotenv"
+
+dotenv.config()
 const app = express();
+
+connectDB();
 
 app.use("/api/notes", notesRoutes);
 
 app.listen(5000);
+
+/*
+
+*/
